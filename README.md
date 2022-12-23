d0cf91e32044558fe1f743f38cafe45e99cf87c7
IceTeaOxO <popopuchang@gmail.com>
2022-12-23 18:52:31 +0800

    解決後端cors問題

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ npm install --save express
 npm install nodemon --save-dev
 npm install -g create-react-app
 npm install --save-dev concurrently
+
+
+npm install -g react-router-dom
+npm install --save react-redux
+npm i react-router
+npm i react-router-dom
+npm i cors
--- server.js
+++ server.js
@@ -1,12 +1,58 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  // do something
+const path = require('path');
+
+// Serve the static files from the React app
+// app.use(express.static(path.join(__dirname, 'client/build')));
+//避免跨域請求被阻擋
+var cors = require('cors');
+app.use(cors());
+const corsOptions = {
+  origin: [
+    'http://www.example.com',
+    'http://localhost:3000',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+// app.use((req, res, next)=> {
+//   res.header('Access-Control-Allow-Origin', "*")
+//   res.header('Access-Control-Allow-Credentials', false)
+//   next()
+// })
+
+
+app.get('/num', function (req, res) {
+  res.send('okkkkkk');
+  console.log("req")
+  // console.log(res);
 })
 
-app.get('/api', function (req, res) {
-    res.send('okkkkkk');
-  })
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+    
+});
+
+
+
+
+
+
+
+app.get('/', function(req, res) {
+  // do something
+  // console.log(req.body)
+  // console.log(req)
+  console.log("////////")
+})
+
+
 
 app.listen(5000);
+console.log('App is listening on port 5000');
